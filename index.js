5f217d36b051ce0b672641cfa9c144fa5a90b9b9
Marie-AnneS <marie-anne.seim@gmail.com>
2019-05-29 16:06:45 +0000

    exercise de recusion

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  
+    console.log(`fromN: ${fromN}`);
+  console.log(`toN: ${toN}`);
+  if (fromN < toN){
+    return fromN += sum(fromN + 1, toN);
+
+  }
+    return fromN;
+  
 }
+console.log(sum(3, 7));
 
 module.exports = sum;
